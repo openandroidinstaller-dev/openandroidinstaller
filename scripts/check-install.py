86e7d0288da2c55f1631e28e0330346555c63e31
Tobias Sterbak <sterbak-it@outlook.com>
2022-09-03 09:16:37 +0200

    Add python script to check adb install

---
+++ scripts/check-install.py
@@ -0,0 +1,15 @@
+"""Check if adb works and print the version."""
+from subprocess import check_output, STDOUT
+
+def check_adb_version():
+    return check_output(['adb', 'version'], stderr=STDOUT).decode()
+
+
+if __name__ == "__main__":
+    print("Checking if adb is installed...")
+    adb_version = check_adb_version()
+    if adb_version:
+        print(adb_version)
+        print("Done.")
+    else:
+        print("Failed.")
