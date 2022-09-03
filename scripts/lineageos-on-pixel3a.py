e39e59e371d0f4f0ff834afdc3bac5fefd69c1b3
Tobias Sterbak <sterbak-it@outlook.com>
2022-09-03 12:08:53 +0200

    First semi-manual run for google pixel 3a, no terminal required

--- scripts/lineageos-on-pixel3a.py
+++ scripts/lineageos-on-pixel3a.py
@@ -1,8 +1,7 @@
 """Script to flash a google pixel 3a.
 
 Example usage:
-
-
+    poetry run python scripts/lineageos-on-pixel3a.py --recovery images/lineage-19.1-20220830-recovery-sargo.img --image images/lineage-19.1-20220830-nightly-sargo-signed.zip 
 """
 import click
 from subprocess import call
@@ -14,7 +13,86 @@ from utils import run_fastboot_command
 @click.option('--recovery', help='Path to the recovery file to flash. (Can be TWRP)')
 @click.option('--image', help='Path to the lineage os image to flash.')
 def install_lineage_os(recovery: str, image: str):
-    unlocking_bootloader_result = unlock_bootloader()
+    # Steps 1: Unlock the bootloader
+    #unlock_result = unlocking_bootloader_result = unlock_bootloader()
+    #if not unlock_result:
+    #    click.echo("Unlocking the bootloader failed. Exiting.")
+    #    return False
+
+    # Step 2: Temporarily booting a custom recovery using fastboot
+    boot_recovery_result = boot_recovery(recovery)
+    if not boot_recovery_result:
+        click.echo("Flashing recovery failed. Exiting.")
+        return False
+
+    # Step 3: Installing LineageOS from recovery
+    install_result = install_os(image)
+    if not install_result:
+        click.echo("Installing LineageOS failed. Exiting.")
+        return False
+
+    click.echo(
+        "Installing lineageOS was successful! Have fun with your device! :)")
+
+
+def install_os(image: str):
+    """Installing LineageOS from recovery with the image filepath given in image."""
+    # reboot into fastboot mode
+    reboot_res = reboot_device_into_bootloader()
+    if reboot_res == 4:
+        click.echo("Booting into bootloader failed. Exiting.")
+        return False
+    # needs screen interaction here
+    confirmed = click.confirm("Select 'Boot into Recovery' on your smartphone screen. Wait until you are in recovery, then confirm",
+                              default=True, abort=False, prompt_suffix=': ', show_default=True, err=False)
+
+    # automatic factory reset
+    #factory_reset_result = run_fastboot_command(cmd=["erase", "cache"])
+    #click.echo(f"{factory_reset_result}")
+    #factory_reset_result = run_fastboot_command(cmd=["erase", "userdata"])
+    #click.echo(f"{factory_reset_result}")
+    
+    # manual factory reset
+    confirmed = click.confirm(
+        "Now tap Factory Reset, then Format data / factory reset and continue with the formatting process. This will remove encryption and delete all files stored in the internal storage, as well as format your cache partition (if you have one). Confirm if you are done",
+        default=True, abort=False, prompt_suffix=': ', show_default=True, err=False
+    )
+
+    # sideload linageos image with adb
+    confirmed = click.confirm(
+        "On the device, select “Apply Update”, then “Apply from ADB” to begin sideload. Then confirm here",
+        default=True, abort=False, prompt_suffix=': ', show_default=True, err=False
+    )
+    click.echo("\nRunning: adb sideload <image>")
+    if call(f'adb sideload {image}', shell=True) < 0:
+        click.echo("*** Sideloading image failed! ***")
+        return False
+    
+    click.echo("Flashing finished. Now press 'back' (arrow) and then 'Reboot system now' to finish the installation.")
+    return True
+
+
+def boot_recovery(recovery: str):
+    """
+    Temporarily booting a custom recovery using fastboot.
+
+    Using the recovery found in the path 'recovery'.
+    """
+    # reboot into fastboot mode
+    reboot_res = reboot_device_into_bootloader()
+    if reboot_res == 4:
+        click.echo("Unlocking the bootloader failed. Exiting.")
+        return False
+    # needs screen interaction here
+    confirmed = click.confirm("Select 'Restart bootloader' on your smartphone screen. Then confirm",
+                              default=True, abort=False, prompt_suffix=': ', show_default=True, err=False)
+    # list devices
+    devices = run_fastboot_command(cmd=["devices"])
+    click.echo(f"Found: {devices}")
+    # flash the recovery (temporarily)
+    flash_result = run_fastboot_command(cmd=["flash", "boot", recovery])
+    click.echo("Recovery flashed successfully.")
+    return True
 
 
 def reboot_device():
@@ -27,12 +105,12 @@ def reboot_device():
 
 
 def reboot_device_into_bootloader():
-    """Reboot the connected device back into fastboot."""
-    click.echo("\nRunning: fastboot reboot-bootloader")
-    if call('fastboot reboot bootloader', shell=True) < 0:
+    """Reboot the connected device into fastboot."""
+    click.echo("\nRunning: adb reboot bootloader")
+    if call('adb reboot bootloader', shell=True) < 0:
         click.echo("*** Reboot-bootloader command failed! ***")
         return 4
-    return 0 
+    return 0
 
 
 def unlock_bootloader():
@@ -42,20 +120,31 @@ def unlock_bootloader():
     if reboot_res == 4:
         click.echo("Unlocking the bootloader failed. Exiting.")
         return False
+    # needs screen interaction here
+    confirmed = click.confirm("Select 'Restart bootloader' on your smartphone screen. Then confirm",
+                              default=True, abort=False, prompt_suffix=': ', show_default=True, err=False)
     # list devices
-    devices = run_fastboot_command(cmd="devices")
+    devices = run_fastboot_command(cmd=["devices"])
     click.echo(f"Found: {devices}")
     # actually unlock the bootloader
-    unlock_res = run_fastboot_command(cmd="flashing unlock")
+    unlock_res = run_fastboot_command(cmd=["flashing", "unlock"])
     click.echo(f"{unlock_res}")
+    click.confirm("At this point the device may display on-screen prompts which will require interaction to continue the process of unlocking the bootloader. Please take whatever actions the device asks you to to proceed.",
+                  default=True, abort=False, prompt_suffix=': ', show_default=True, err=False
+                  )
     # reboot device
     reboot_res = reboot_device()
     if reboot_res == 4:
         click.echo("Unlocking the bootloader failed while final reboot. Exiting.")
         return False
     click.echo("Bootloader is now unlocked!")
-    click.echo(">>Since the device resets completely, you will need to re-enable USB debugging to continue.")
-
+    click.echo(
+        ">>Since the device resets completely, you will need to re-enable USB debugging to continue.")
+    confirmed = click.confirm(
+        "Confirm to continue",
+        default=True, abort=False, prompt_suffix=': ', show_default=True, err=False
+    )
+    return confirmed
 
 
 if __name__ == '__main__':
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,7 +1,8 @@
 """Reusable functions for flashing."""
+from typing import List
 from subprocess import check_output, STDOUT
 
 
-def run_fastboot_command(cmd: str) -> str:
+def run_fastboot_command(cmd: List[str]) -> str:
     """Run a fastboot command and return the result as string."""
-    return check_output(['fastboot', cmd], stderr=STDOUT).decode()
+    return check_output(['fastboot'] + cmd, stderr=STDOUT).decode()
