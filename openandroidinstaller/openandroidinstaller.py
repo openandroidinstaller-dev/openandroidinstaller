7cf965ae3ca41595cebc61d32cbd890aafb9f870
Tobias Sterbak <sterbak-it@outlook.com>
2022-09-06 11:52:59 +0200

    First flow of the app

--- openandroidinstaller/openandroidinstaller.py
+++ openandroidinstaller/openandroidinstaller.py
@@ -1,52 +1,79 @@
-"""OpenAndroidInstsaller main app."""
-from time import sleep
 import flet
-from flet import Page, Row, TextField, Image, CircleAvatar, Column, ProgressRing
-from flet import (ElevatedButton, FilePicker, FilePickerResultEvent, Page, Row, Text, icons)
+from flet import AppBar, ElevatedButton, Page, Text, View, colors, FloatingActionButton
 
 
 def main(page: Page):
-    # HEADER part
     page.title = "OpenAndroidInstaller"
-    page.theme_mode = "dark"
-    page.padding = 30
+    views = []    
+
+
+    def go_next(e):
+        view_num = int(page.views[-1].route) + 1
+        if view_num > 3:
+            view_num = 3
+        page.views.clear()
+        page.views.append(views[view_num])
+        page.update()
+
+    def go_back(e):
+        view_num = int(page.views[-1].route) - 1
+        if view_num < 0:
+            view_num = 0
+        page.views.clear()
+        page.views.append(views[view_num])
+        page.update()
+
+
+    def get_welcome_view(page: Page) -> View:
+        return View(
+            "0",
+            [
+                AppBar(title=Text("Welcome to OpenAndroidInstaller!")),
+                ElevatedButton("Next", on_click=go_next),
+            ],
+        )
+
+    def get_bootloader_view(page: Page) -> View:
+        return View(
+            "1",
+            [
+                AppBar(title=Text("Unlock bootloder")),
+                ElevatedButton("Next", on_click=go_next),
+                ElevatedButton("Back", on_click=go_back),
+            ],
+        )
+
+    
+    def get_image_select_view(page: Page) -> View:
+        return View(
+            "2",
+            [
+                AppBar(title=Text("Select images.")),
+                ElevatedButton("Next", on_click=go_next),
+                ElevatedButton("Back", on_click=go_back),
+            ],
+        )
+
+
+    def get_recovery_view(page: Page) -> View:
+        return View(
+            "3",
+            [
+                AppBar(title=Text("Boot recovery")),
+                ElevatedButton("Next", on_click=go_next),
+                ElevatedButton("Back", on_click=go_back),
+            ],
+        )
+
+    views = [
+        get_welcome_view(page),
+        get_bootloader_view(page),
+        get_image_select_view(page),
+        get_recovery_view(page)
+    ]
+
+    page.views.append(views[0])
     page.update()
 
-    img = Image(
-        src="logo-192x192.png",
-        width=100,
-        height=100,
-        fit="contain",
-    )
-    page.add(img)
 
-    ### END HEADER
-
-    spinner = Column(
-            [ProgressRing(), Text("I'm going to run for ages...")],
-            horizontal_alignment="center",
-        )
-    
-    page.add(spinner)
-
-    page.add(
-        Row(controls=[
-            Text("A"),
-            Text("B"),
-            Text("C")
-        ])
-    )
-
-    name_field = TextField(label="Your name")
-
-    def button_clicked(e):
-        page.add(Text(f"Your name is {name_field.value}"))
-
-    page.add(
-        Row(controls=[
-            name_field,
-            ElevatedButton(text="Say my name!", on_click=button_clicked)
-        ])
-    )
-
-flet.app(target=main, assets_dir="assets")
+flet.app(target=main)
