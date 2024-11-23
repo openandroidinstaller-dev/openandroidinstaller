897b91f108148a8dbcea731c6653d6ceaddde416
SirRGB <sirrgb@proton.me>
2024-11-23 17:52:17 +0100

    update lineage file name example to version 21
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- openandroidinstaller/views/select_view.py
+++ openandroidinstaller/views/select_view.py
@@ -245,7 +245,7 @@ OpenAndroidInstaller works with the [TWRP recovery project](https://twrp.me/abou
                 Text("Select an OS image:", style="titleSmall"),
                 Markdown(
                     f"""
-The image file should look something like `lineage-20.0-20240101-nightly-{self.state.config.device_code}-signed.zip`."""
+The image file should look something like `lineage-21.0-20241101-nightly-{self.state.config.device_code}-signed.zip`."""
                 ),
                 Row(
                     [
